f20cbde474f1a6342e37fb7ca05d1a553a2b386d
Lisa Le <lisale841@gmail.com>
2022-08-19 16:39:01 -0700

    routes

---
+++ src/components/animals/Animals.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Animals = () =>{
+  return (
+    <div>
+      <h3>Dogs</h3>
+      <div>
+        <h1>hello</h1>
+        <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.freepik.com%2Fpremium-vector%2Fcute-dog-drinking-boba-tea_295036-691.jpg&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcute-dog-drinking-boba-tea_14941197.htm&tbnid=7WhQ3Ql_Qbv0cM&vet=12ahUKEwij_rC3_dP5AhW1omoFHRIuDgsQMygDegUIARDRAg..i&docid=wbZGCOnpzdqijM&w=626&h=626&q=dog%20boba%20cute&ved=2ahUKEwij_rC3_dP5AhW1omoFHRIuDgsQMygDegUIARDRAg" alt="dog"/>
+        <img src={"./dog-image.jpg"} alt="dog"></img>
+      </div>
+    </div>
+  );
+}
+export default Animals;
