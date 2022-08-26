bdead8eaf25c8b4d05d5377624a61c0d94961552
Lisa Le <lisale841@gmail.com>
2022-08-26 11:09:31 -0700

    fixed multiple routing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import NavBar from "./components/navBar/navBar"
 import Animals from "./components/animals/Animals"
-import HomePage from "./components/homePage/Homepage"
+import HomePage from "./components/homePage/HomePage"
 function App() {
   return (
     <div className="container">
     <div>
     <Router>
       <Routes>
-        <Route path='/' element={<NavBar />} >
-           <Route path='/' element={<HomePage/>} />
-          <Route path='animals' element={<Animals/>} />
+        <Route path='/' element={<><NavBar/><HomePage/></>} >
+        <Route path='animals' element={<Animals/>} />
         </Route>
       </Routes>
     </Router>
