830881da630b4d87a8f47e2eec8bd45ccf5d2602
Lisa Le <lisale841@gmail.com>
2022-08-26 16:47:48 -0700

    homepage update

--- src/components/homePage/Home.js
+++ src/components/homePage/Home.js
@@ -5,11 +5,11 @@ const Home = () =>{
     <div className='homeBackground'>
      <div className='homeWrapper'>
        <div>
-          <img src={require('../../images/cat-image.jpg')} alt= "cat"/>
-          <img src={require('../../images/dog-image.jpg')} alt= "dog"/>
+          <img src={require('../../images/vera-profile1.jpg')} alt= "vera"/>
+          {/* <img src={require('../../images/dog-image.jpg')} alt= "dog"/>
           <img src={require('../../images/fox-image.jpg')} alt= "fox"/>
           <img src={require('../../images/koala-image.png')} alt= "koala"/>
-          <img src={require('../../images/panda-image.jpg')} alt= "panda"/>
+          <img src={require('../../images/panda-image.jpg')} alt= "panda"/> */}
         </div>
       </div>
     </div>
